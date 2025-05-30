4ef28747ee76ec48d3380c53ac4df53e92fa0c61
zunairahmehmood <shahzanamehmood@gmail.com>
2025-05-30 12:58:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+// chapter #01
+// alert
+alert("Error! PLease enter a valid password.");
+
+// line break
+alert("Welcome to JS Land...\nHappy Coding!");
+
+
+// seqence
+alert("Welcome to JS Land...");
+
+alert("Happy Coding!");
+
+// answer of 7 is (c)
+
+
+
+
+
+
+
+
+// chapter #02
+// variable for string
+
+// question no 1
+var username = "zunairah__mehmood";
+
+// question no 2
+var myName = "Zunaira Mehmood";
+
+// question no 3
+var message = "Hello World";
+alert(message);
+
+// question no 4
+var name = "john doe";
+var myAge = "15 years old";
+var course = "Certified Mobile Application Development";
+
+alert(name);
+alert(myAge);
+alert(course);
+
+// question no 5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+// question no 6
+var email = "my email address is example@example.com";
+alert(email);
+
+// question no 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// question no 8
+var html = "Yah! I can write HTML content through JavaScript";
+document.write(html);
+
+// question no 9
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
